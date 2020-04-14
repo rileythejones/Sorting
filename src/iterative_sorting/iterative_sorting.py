ff991940d4365f5e5320d1b2e95aadafd238b0d5
rileythejones <rileymjones@gmail.com>
2020-04-14 17:11:29 -0600

    merge sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,15 +1,13 @@
 # TO-DO: Complete the selection_sort() function below 
+
 def selection_sort(arr):
-    i = 0 
-    for i in range(0, len(arr)):
-        j = 1
-        for j in range(0, len(arr)):
-            if j > 0 and arr[j-1] > arr[j]:
-                temp = arr[j]
-                arr[j] = arr[j-1]
-                arr[j-1] = temp
-            j -= 1 
-        i += i 
+    for i in range(0, len(arr) -1):
+        cur_index = i
+        smallest_index = cur_index
+        for j in range (cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     return arr
 
 # TO-DO:  implement the Bubble Sort function below
@@ -24,3 +22,44 @@ def bubble_sort(a):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
+
+
+""" selection sort """ 
+# swap value at the smallest index with our starting index 
+
+# [1, 2, 3, 4]       [6, 4, 8, 4, 2 , 1, 9]
+# sorted side         ^current        ^smallest
+
+
+
+
+""" find the index of the smallest value """
+for j in range (cur_index, len(arr)):
+    if arr[j] < arr[smallest_index]:
+        smallest_index = j
+
+""" swamp the current index with the smallest index """
+
+for i in range(0, len(arr) -1):
+    cur_index = i
+    smallest_index = cur_index
+    arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+
+
+""" bubble sort """
+
+
+
+"""largest values "bubble to the top" one by one  """
+
+def bubble_sort(arr):
+    swaps_occured = True
+    while swaps_occured:
+        print(arr)
+        swaps_occured = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i] # SWAP from the right 
+                swaps_occured = True 
